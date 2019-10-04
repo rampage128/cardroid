3e8330283c435c01e2d7dd7fdf91331046d4f04d
rampage128 <frederik.wolter@googlemail.com>
2019-10-04 04:21:57 +0200

    Fix detection timeout triggering even after successful device detection

--- app/src/main/java/de/jlab/cardroid/devices/usb/serial/UsbSerialDeviceDetector.java
+++ app/src/main/java/de/jlab/cardroid/devices/usb/serial/UsbSerialDeviceDetector.java
@@ -6,17 +6,19 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import androidx.annotation.NonNull;
+import de.jlab.cardroid.devices.DeviceHandler;
 import de.jlab.cardroid.devices.DeviceService;
 import de.jlab.cardroid.devices.usb.UsbDeviceDetector;
 
 public abstract class UsbSerialDeviceDetector extends UsbDeviceDetector {
 
-    private Timer timer = new Timer();
+    private Timer timer;
 
     @Override
     protected boolean startIdentification(@NonNull UsbDevice device, @NonNull DeviceService service) {
         boolean connectionSuccess = this.startSerialIdentification(device, service);
         if (connectionSuccess) {
+            this.timer = new Timer();
             this.timer.schedule(new TimerTask() {
                 @Override
                 public void run() {
@@ -25,6 +27,11 @@ public abstract class UsbSerialDeviceDetector extends UsbDeviceDetector {
             }, this.getTimeout());
         }
         return connectionSuccess;
+    }
+
+    protected void deviceDetected(@NonNull DeviceHandler handler) {
+        this.timer.cancel();
+        super.deviceDetected(handler);
     }
 
     protected abstract boolean startSerialIdentification(@NonNull UsbDevice device, @NonNull DeviceService service);
