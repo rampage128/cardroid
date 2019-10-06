0ab55c0d385237af0333379fdfcf207a545502d3
rampage128 <frederik.wolter@googlemail.com>
2019-10-06 23:48:17 +0200

    Fix Exception due to context being shared in different threads

--- app/src/main/java/de/jlab/cardroid/variables/ScriptEngine.java
+++ app/src/main/java/de/jlab/cardroid/variables/ScriptEngine.java
@@ -1,5 +1,7 @@
 package de.jlab.cardroid.variables;
 
+import android.util.Log;
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
@@ -19,13 +21,23 @@ public class ScriptEngine {
     }
 
     public Expression createExpression(String expression, ObservableValueBag variables, String contextName) {
-        Context context = Context.enter();
-        Scriptable scope = context.initStandardObjects();
+        Scriptable scope = this.context.initStandardObjects();
         return new Expression(expression, variables, contextName, scope, this);
     }
 
-    Object evaluate(String expression, String contextName, Scriptable scope) {
-        return context.evaluateString(scope, expression, contextName, 1, null);
+    public Object evaluate(String expression, String contextName, Scriptable scope) {
+        Context context = Context.enter();
+        context.setOptimizationLevel(-1); // prevent JVM bytecode generation
+        Object result = 0L;
+        try {
+            result = context.evaluateString(scope, expression, contextName, 1, null);
+        } catch (Exception e) {
+            Log.e(this.getClass().getSimpleName(), "Error evaluating \"" + expression + "\"", e);
+        } finally {
+            Context.exit();
+        }
+
+        return result;
     }
 
 }
