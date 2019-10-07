122d3f52b0e3d00b78fcc11ee14b8d3f7529e013
rampage128 <frederik.wolter@googlemail.com>
2019-10-07 02:15:13 +0200

    Fix byte mask and bit offset calculation errors (and add logging)

--- app/src/main/java/de/jlab/cardroid/car/CanPacketDescriptor.java
+++ app/src/main/java/de/jlab/cardroid/car/CanPacketDescriptor.java
@@ -1,5 +1,6 @@
 package de.jlab.cardroid.car;
 
+import android.util.Log;
 import android.util.SparseIntArray;
 
 import java.util.ArrayList;
@@ -23,8 +24,8 @@ public class CanPacketDescriptor implements CanDeviceHandler.CanPacketListener {
     public void addCanValue(@NonNull CanValue value) {
         this.values.add(value);
 
-        int startByteIndex = (int)Math.floor((value.getBitIndex() + 1) / 8f) - 1;
-        int endByteIndex = startByteIndex + (int)Math.floor(value.getBitLength() / 8);
+        int startByteIndex = (int)Math.floor((value.getBitIndex() + 1) / 8f);
+        int endByteIndex = startByteIndex + (int)Math.floor((value.getBitLength() - 1) / 8);
 
         this.recalculateByteMask(startByteIndex, endByteIndex);
         this.compressedPacketLength = getByteCountUntil(8);
@@ -32,27 +33,43 @@ public class CanPacketDescriptor implements CanDeviceHandler.CanPacketListener {
     }
 
     private void recalculateByteMask(int startByteIndex, int endByteIndex) {
-        for (int i = startByteIndex; i < endByteIndex; i++) {
+        for (int i = startByteIndex; i <= endByteIndex; i++) {
             this.byteMask |= 1 << (7 - i);
         }
+
+        StringBuilder sb = new StringBuilder("Recalculated byte mask for " + String.format("%02x", this.getCanId()) + " (" + startByteIndex + " -> " + endByteIndex + "): ");
+        for (int i = 0; i < 8; i++) {
+            sb.append((this.byteMask >> (7 - i)) & 0x01);
+        }
+        Log.e(this.getClass().getSimpleName(), sb.toString());
     }
 
     private void recalculateOffsets() {
+        Log.e(this.getClass().getSimpleName(), "Recalculating bit offsets for: " + String.format("%02x", this.getCanId()));
         for (int i = 0; i < this.values.size(); i++) {
             CanValue value = this.values.get(i);
             int bitIndex = value.getBitIndex();
             int startByteIndex = (int)Math.floor((bitIndex + 1) / 8f);
-            int byteCount = getByteCountUntil(startByteIndex);
-            int byteOffset = byteCount - startByteIndex;
-            int bitOffset = (byteOffset * 8) * -1;
+            int byteCount = getUnusedByteCountUntil(startByteIndex);
+            int byteOffset = byteCount * -1;
+            int bitOffset = (byteOffset * 8);
             this.bitOffsets.put(value.getBitIndex(), bitOffset);
+            Log.e(this.getClass().getSimpleName(), "Recalculating bit offset: " + bitIndex + " " + bitOffset + " = " + (bitIndex + bitOffset) + " (start byte: " + startByteIndex + ", unused bytes before: " + byteCount + ", byte offset: " + byteOffset + ")");
         }
     }
 
     private int getByteCountUntil(int endByteIndex) {
         int byteCount = 0;
         for (int i = 0; i < endByteIndex; i++) {
-            byteCount += (this.byteMask >> i) & 0x01;
+            byteCount += (this.byteMask >> (7 - i)) & 0x01;
+        }
+        return byteCount;
+    }
+
+    private int getUnusedByteCountUntil(int endByteIndex) {
+        int byteCount = 0;
+        for (int i = 0; i < endByteIndex; i++) {
+            byteCount += (this.byteMask >> (7 - i)) & 0x01 ^ 0x01;
         }
         return byteCount;
     }
