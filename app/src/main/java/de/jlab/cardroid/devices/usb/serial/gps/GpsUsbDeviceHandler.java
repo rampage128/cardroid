7ef29cdfcc3c7a8501e57d6ecdb6f8dec6062b29
rampage128 <frederik.wolter@googlemail.com>
2019-10-10 19:57:32 +0200

    Fix wrong provider used for gps serial devices

--- app/src/main/java/de/jlab/cardroid/devices/usb/serial/gps/GpsUsbDeviceHandler.java
+++ app/src/main/java/de/jlab/cardroid/devices/usb/serial/gps/GpsUsbDeviceHandler.java
@@ -4,10 +4,10 @@ import android.app.Application;
 import android.hardware.usb.UsbDevice;
 
 import androidx.annotation.NonNull;
-import de.jlab.cardroid.car.CanDataProvider;
 import de.jlab.cardroid.devices.serial.gps.GpsPositionParser;
 import de.jlab.cardroid.devices.serial.gps.GpsSerialReader;
 import de.jlab.cardroid.devices.usb.serial.UsbSerialDeviceHandler;
+import de.jlab.cardroid.gps.GpsDataProvider;
 
 public final class GpsUsbDeviceHandler extends UsbSerialDeviceHandler<GpsSerialReader> {
 
@@ -21,7 +21,7 @@ public final class GpsUsbDeviceHandler extends UsbSerialDeviceHandler<GpsSerialR
     protected GpsSerialReader onConnect() {
         GpsSerialReader reader = new GpsSerialReader();
         reader.addSerialPacketListener(this.positionParser);
-        this.notifyFeatureDetected(CanDataProvider.class, this.positionParser, null);
+        this.notifyFeatureDetected(GpsDataProvider.class, this.positionParser, null);
         return reader;
     }
 
