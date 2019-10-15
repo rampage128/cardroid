bb16bf1687e644f72d589e6b056f4aacf2897b42
Num Mendez <num@jacoti.com>
2019-10-15 23:29:34 +0100

    stub the UsbTv007 detector

---
+++ app/src/main/java/de/jlab/cardroid/devices/usb/camera/UsbTv007DeviceDetector.java
@@ -0,0 +1,16 @@
+package de.jlab.cardroid.devices.usb.camera;
+
+import android.hardware.usb.UsbDevice;
+
+import androidx.annotation.NonNull;
+
+import de.jlab.cardroid.devices.DeviceService;
+import de.jlab.cardroid.devices.usb.UsbDeviceDetector;
+
+public class UsbTv007DeviceDetector extends UsbDeviceDetector {
+
+    @Override
+    protected boolean startIdentification(@NonNull UsbDevice device, @NonNull DeviceService service) {
+        
+    }
+}
