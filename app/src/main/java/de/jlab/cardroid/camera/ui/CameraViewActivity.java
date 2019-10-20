4333f12bedb894b2386196db0400a08035bb293b
Num Mendez <num@jacoti.com>
2019-10-20 17:12:39 +0100

    add camera activity + enable the detector

---
+++ app/src/main/java/de/jlab/cardroid/camera/ui/CameraViewActivity.java
@@ -0,0 +1,96 @@
+package de.jlab.cardroid.camera.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.hardware.camera2.CameraAccessException;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import de.jlab.cardroid.R;
+import de.jlab.cardroid.camera.CameraDataProvider;
+import de.jlab.cardroid.devices.DeviceService;
+
+public class CameraViewActivity extends AppCompatActivity implements CameraDataProvider.CameraProviderListener, GLSurfaceView.Renderer {
+
+    private GLSurfaceView cameraView;
+    private CameraDataProvider cameraProvider;
+
+    private DeviceService.DeviceServiceBinder serviceBinder;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            CameraViewActivity.this.serviceBinder = (DeviceService.DeviceServiceBinder) service;
+            CameraViewActivity.this.bindDataProvider();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            CameraViewActivity.this.unbindDataProvider();
+            CameraViewActivity.this.serviceBinder = null;
+        }
+    };
+
+    private void bindDataProvider() {
+        cameraProvider = this.serviceBinder.getDeviceProvider(CameraDataProvider.class);
+        if (cameraProvider != null) {
+            cameraProvider.setFrameListener(this);
+        }
+    }
+
+    private void unbindDataProvider() {
+        cameraProvider.setFrameListener(null);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_view);
+        cameraView = findViewById(R.id.camera_view);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.getApplicationContext().unbindService(this.serviceConnection);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.getApplicationContext().bindService(new Intent(this.getApplicationContext(), DeviceService.class), this.serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onFrameUpdate() {}
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        cameraProvider.attachToGLContext(GLES20.GL_TEXTURE1);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        //Center the screen in a 4/3 ratio
+        // TODO: we should get access to the current video stream frame ratio
+        int maxWidth = Math.min(height * 4 / 3, width);
+        int maxHeight = maxWidth * 3 / 4;
+        GLES20.glViewport((width - maxWidth) / 2, (height - maxHeight) / 2, maxWidth, maxHeight);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Clear the color buffer
+        // TODO: Is this the cause of the initial swirl?
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        // TODO: maybe we should get the actual rendered frame (if there was any)
+        cameraProvider.updateImage();
+    }
+}
--- app/src/main/java/de/jlab/cardroid/devices/usb/camera/UsbTv007DeviceDetector.java
+++ app/src/main/java/de/jlab/cardroid/devices/usb/camera/UsbTv007DeviceDetector.java
@@ -1,8 +1,13 @@
 package de.jlab.cardroid.devices.usb.camera;
 
+import android.app.Application;
 import android.hardware.usb.UsbDevice;
 
 import androidx.annotation.NonNull;
+
+import com.arksine.libusbtv.UsbTv;
+
+import java.util.ArrayList;
 
 import de.jlab.cardroid.devices.DeviceService;
 import de.jlab.cardroid.devices.usb.UsbDeviceDetector;
@@ -11,6 +16,9 @@ public class UsbTv007DeviceDetector extends UsbDeviceDetector {
 
     @Override
     protected boolean startIdentification(@NonNull UsbDevice device, @NonNull DeviceService service) {
-        return false;
+        ArrayList<UsbDevice> devList = UsbTv.enumerateUsbtvDevices(service.getBaseContext());
+        boolean detected = devList.contains(device);
+        this.deviceDetected(new UsbTv007(device, service.getApplication()));
+        return detected;
     }
 }
