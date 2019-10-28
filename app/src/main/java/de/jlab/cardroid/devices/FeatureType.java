0643c7adc106cb14abf6d23f68775b6f8a27586f
rampage128 <frederik.wolter@googlemail.com>
2019-10-28 01:09:52 +0100

    Fix features not being listed in DeviceDetailFragment

--- app/src/main/java/de/jlab/cardroid/devices/FeatureType.java
+++ app/src/main/java/de/jlab/cardroid/devices/FeatureType.java
@@ -9,8 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import de.jlab.cardroid.R;
+import de.jlab.cardroid.car.CanInteractable;
 import de.jlab.cardroid.car.CanObservable;
 import de.jlab.cardroid.car.ui.CarMonitorActivity;
+import de.jlab.cardroid.devices.serial.carduino.EventInteractable;
 import de.jlab.cardroid.devices.serial.carduino.EventObservable;
 import de.jlab.cardroid.devices.storage.DeviceEntity;
 import de.jlab.cardroid.errors.ErrorObservable;
@@ -20,11 +22,13 @@ import de.jlab.cardroid.rules.RuleActivity;
 
 public enum FeatureType {
 
-    UNKNOWN_FEATURE(Feature.class, null, R.string.device_feature_unknown, R.string.device_feature_unknown_description, R.drawable.ic_device_feature_unknown),
     LOCATION_FEATURE(GpsObservable.class, GpsMonitorActivity.class, R.string.device_feature_location, R.string.device_feature_location_description, R.drawable.ic_device_feature_location),
-    EVENT_FEATURE(EventObservable.class, RuleActivity.class, R.string.device_feature_events, R.string.device_feature_events_description, R.drawable.ic_device_feature_events),
+    EVENT_FEATURE_RECEIVE(EventInteractable.class, RuleActivity.class, R.string.device_feature_events_receive, R.string.device_feature_events_receive_description, R.drawable.ic_device_feature_events),
+    EVENT_FEATURE_SEND(EventObservable.class, RuleActivity.class, R.string.device_feature_events_send, R.string.device_feature_events_send_description, R.drawable.ic_device_feature_events),
     ERROR_FEATURE(ErrorObservable.class, null, R.string.device_feature_errors, R.string.device_feature_errors_description, R.drawable.ic_device_feature_errors),
-    CAN_FEATURE(CanObservable.class, CarMonitorActivity.class, R.string.device_feature_can, R.string.device_feature_can_description, R.drawable.ic_device_feature_can);
+    CAN_FEATURE_LISTEN(CanObservable.class, CarMonitorActivity.class, R.string.device_feature_can, R.string.device_feature_can_description, R.drawable.ic_device_feature_can),
+    CAN_FEATURE_INTERACT(CanInteractable.class, CarMonitorActivity.class, R.string.device_feature_can_interact, R.string.device_feature_can_interact_description, R.drawable.ic_device_feature_can),
+    UNKNOWN_FEATURE(Feature.class, null, R.string.device_feature_unknown, R.string.device_feature_unknown_description, R.drawable.ic_device_feature_unknown);
 
     private Class<? extends Feature> featureClass;
     @StringRes
@@ -79,10 +83,16 @@ public enum FeatureType {
 
     @NonNull
     public static FeatureType get(@NonNull String providerClassName) {
-        for (FeatureType type : FeatureType.values()) {
-            if (type.featureClass.getSimpleName().equals(providerClassName)) {
-                return type;
+        try {
+            Class<?> featureClass = Class.forName(providerClassName);
+
+            for (FeatureType type : FeatureType.values()) {
+                if (type.featureClass.isAssignableFrom(featureClass)) {
+                    return type;
+                }
             }
+        } catch (Exception e) {
+            // NOTHING TO DO HERE
         }
         return UNKNOWN_FEATURE;
     }
