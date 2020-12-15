3469620ca4e331a52a8ff52c20a9e75feb614452
rampage128 <frederik.wolter@googlemail.com>
2020-12-15 18:41:39 +0100

    Fix carduino ID pattern matching for multi line matching

--- app/src/main/java/de/jlab/cardroid/devices/serial/carduino/CarduinoUidGenerator.java
+++ app/src/main/java/de/jlab/cardroid/devices/serial/carduino/CarduinoUidGenerator.java
@@ -18,11 +18,11 @@ public final class CarduinoUidGenerator {
     private static final String ID_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String ID_PATTERN_PART = "[A-Z]{" + ID_LENGTH + "}";
     private static final String ID_PATTERN = "^" + ID_PATTERN_PART + "$";
-    private static final String FRAME_ID_PATTERN = new StringBuilder(".*")
+    private static final String FRAME_ID_PATTERN = new StringBuilder("[\\S\\s]*")
             .append("\\").append((char)CarduinoSerialPacket.HEADER)
             .append((char)CarduinoPacketType.META.getType())
             .append("[^\\}]*(").append(ID_PATTERN_PART).append(")\\").append((char)CarduinoSerialPacket.FOOTER)
-            .append(".*").toString();
+            .append("[\\S\\s]*").toString();
 
     @NonNull
     public static String generateId(@NonNull Application app) {
